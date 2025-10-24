87ea9b606b71c83cfb7f54d16edaa2f42bd43c3f
shacortes <sergihui@outlook.es>
2025-10-24 02:51:52 +0200

    Add insult-response game implementation
    - Created Main class with a game that allows players to respond to pirate insults.
    - Introduced arrays for insults and player responses, ensuring they match in length.
    - Implemented game logic for rounds, including input validation and tracking lives.
    - Added randomization for insult selection and response options.
    - Provided feedback for correct and incorrect answers, along with remaining lives.

--- prou2ex03/src/main/java/es/cide/programacio/Main.java
+++ prou2ex03/src/main/java/es/cide/programacio/Main.java
@@ -8,22 +8,14 @@ public class Main {
     public static void main(String[] args) {
         Random insultsAleatoris = new Random();
         Random respostesIncorrectes = new Random();
-        Random locuraNumero3 = new Random();
-        //explicar variables que no entenc res
+        // Random locuraNumero3 = new Random(); 
+        Random rnd = new Random();
+        Scanner sc = new Scanner(System.in);
         int vides = 3;
         char continuacio = 'y';
-        Scanner sc = new Scanner(System.in);
-        int respIncAleat = 0;
         int insult = 0;
-        int resposta = 0;
         int respostaNumJugador = -1;
-        boolean cercantResposta = true;
-        int respostesTrobades = 0;
-        int indexResposteMostrades = 0;
-        int noTeNom = 0;
-        String respostaString = "Hola";
-        int respostesMostJug = 0;
-        int mecachisRespostaAleatoriaIndexCompANoTeNom = 0;
+        int puntuacio = 0;
         String[] insults = {"¡Has dejado ya de usar pañales?",
             "¡No hay palabras para describir lo asqueroso que eres!",
             "¡He hablado con simios más educados que tú!",
@@ -44,69 +36,73 @@ public class Main {
             "Y yo tengo un SALUDO para ti, ¿te enteras?",
             "Te habrá enseñado todo lo que sabes.",
             "¿TAN rápido corres?"};
-        String[] respostesMostrades = {"a", "b", "c"};
-
-        /*String[] respostesIncorrectes = {"¿Por qué? ¿Acaso querías pedir uno prestado?",
-            "Sí que las hay, sólo que nunca las has aprendido.",
-            "Me alegra que asistieras a tu reunión familiar diaria.",
-            "Primero deberías dejar de usarla como un plumero.",
-            "Qué apropiado, tú peleas como una vaca.",
-            "Ya te están fastidiando otra vez las almorranas, ¿eh?",
-            "Ah, ¿ya has obtenido ese trabajo de barrendero?",
-            "Y yo tengo un SALUDO para ti, ¿te enteras?",
-            "Te habrá enseñado todo lo que sabes.",
-            "¿TAN rápido corres?"};*/
+        boolean[] usat = new boolean[insults.length];
         System.out.println("Quantes rondes vols jugar? Si vols sortir prem 0");
         int rondes = sc.nextInt();
         if (rondes >= 0) {
             System.out.println("Començem el Joc");
-            for (int i = 0; i < rondes && continuacio != 'n' && vides > 0; i++) {
-                insult = insultsAleatoris.nextInt(9);
+            for (int i = 0; i < rondes && continuacio != 'n' && vides > 0 && i < insults.length; i++) {
+                do {
+                    insult = insultsAleatoris.nextInt(insults.length);
+                } while (usat[insult]);
+                usat[insult] = true;
                 System.out.println("Pirata: " + insults[insult]);
-                respostaString = insults[insult];
                 System.out.println("Tria la teva resposta!");
-                System.out.println("Jugador: " + respostesJugador[insult]);
-                respostesMostrades[0] = respostesJugador[insult];
-                while (cercantResposta && respostesTrobades < 2) {
-                    respIncAleat = respostesIncorrectes.nextInt(9);
-                    if (respIncAleat != insult) {
-                        System.out.println(respostesJugador[respIncAleat]);
-                        respostesMostrades[indexResposteMostrades + 1] = respostesJugador[respIncAleat];
-                        respostesTrobades++;
+                int[] opcions = new int[3];
+                opcions[0] = insult;
+                int count = 1;
+                while (count < 3) {
+                    int aleat = respostesIncorrectes.nextInt(respostesJugador.length);
+                    boolean jaExisteix = false;
+
+                    for (int indexArraySort = 0; indexArraySort < count; indexArraySort++) {
+                        if (opcions[indexArraySort] == aleat || aleat == insult) {
+                            jaExisteix = true;
+                            break;
+                        }
+                    }
+                    if (!jaExisteix) {
+                        opcions[count] = aleat;
+                        count++;
                     }
                 }
-                //randomitzar respostes
-                noTeNom = locuraNumero3.nextInt(respostesMostrades.length);
-                while (respostesMostJug <= 2){
-                    if (noTeNom != mecachisRespostaAleatoriaIndexCompANoTeNom && noTeNom != insult){
-                        System.out.println(respostesMostrades[noTeNom]);
-                        respostesMostJug++;
-                        noTeNom = locuraNumero3.nextInt(respostesMostrades.length);
-                    } else if (noTeNom == mecachisRespostaAleatoriaIndexCompANoTeNom || noTeNom == insult){
-                        noTeNom = locuraNumero3.nextInt(respostesMostrades.length);
-                    }
+                for (int j = 0; j < 3; j++) {
+                    int k = rnd.nextInt(3);
+                    int temp = opcions[j];
+                    opcions[j] = opcions[k];
+                    opcions[k] = temp;
                 }
-                
-
-
-
-
-
-
-
-
-
-
+                for (int j = 0; j < 3; j++) {
+                    System.out.println((j + 1) + ": " + respostesJugador[opcions[j]]);
+                }
                 respostaNumJugador = sc.nextInt();
+                if (opcions[respostaNumJugador - 1] == insult) {
+                    System.out.println("Correcte!");
+                    puntuacio++;
+                } else {
+                    System.out.println("Incorrecte!");
+                    vides--;
+                }
+                System.out.println("Vides restants: " + vides);
+                System.out.println("--------------------------------------\n");
+            }
+            if (vides == 0) {
+                System.out.println("Has perdut totes les vides. Fi del joc!");
+            } else {
+                System.out.println("Has acabat el joc amb " + vides + " vides restants.");
+                if (puntuacio > 8) {
+                    System.out.println("Ets el rei dels pirates");
+                } else if (puntuacio > 4) {
+                    System.out.println("Ets un bon pirata");
+                } else {
+                    System.out.println("Els lloros riuen de tu!");
 
-
+                }
             }
         } else if (rondes < 0) {
             System.out.println("El nombre de rondes ha de ser positiu.");
         } else if (rondes == 0) {
             System.out.println("Fins aviat!");
         }
-
     }
-
 }
